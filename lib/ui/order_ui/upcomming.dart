516809e9f22364bffe7f82f8416955a6dacf2e18
kk40401637 <kaungkhantzin123@gmail.com>
2022-08-23 20:08:48 +0630

    first commit

---
+++ lib/ui/order_ui/upcomming.dart
@@ -0,0 +1,110 @@
+import 'package:apitestinglogin/ui/order_ui/map_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../../models/orderListModel/new_order_model.dart';
+
+class Upcomming extends StatelessWidget {
+  List<NewOrderResultModel> orderList;
+  Function(String orderId, int changeStatus) updateStatus;
+  Upcomming(this.orderList, {required this.updateStatus});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        padding: const EdgeInsets.all(20.0),
+        itemCount: orderList.length,
+        itemBuilder: (context, index) {
+          NewOrderResultModel model = orderList[index];
+          if (model.riderOrderStatus == "1") {
+            return Card(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Text(model.orderNo!),
+                        const Spacer(),
+                        Container(
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                              color: Colors.green,
+                              borderRadius: BorderRadius.circular(10.0)),
+                          child: Text(
+                            "Ready at ${DateTime.parse(model.orderDateTime!).hour}:${DateTime.parse(model.orderDateTime!).minute}",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      "Rider Name",
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                    ),
+                    Text(model.orderDetail!),
+                    Text(model.restaurantName!),
+                    Text("${model.orderTotalAmount} item(s)"),
+                    Row(
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => MapScreen(),
+                              ),
+                            );
+                          },
+                          child: Icon(
+                            Icons.map,
+                            color: Color(0xFFF55F01),
+                            size: 30,
+                          ),
+                        ),
+                        Spacer(),
+                        OutlinedButton(
+                            style: OutlinedButton.styleFrom(
+                              side: BorderSide(
+                                width: 2.0,
+                                color: Color(0xFFF55F01),
+                              ),
+                            ),
+                            onPressed: () {},
+                            child: Text(
+                              'Reject',
+                              style: TextStyle(
+                                color: Color(0xFFF55F01),
+                              ),
+                            )),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              primary: Color(0xFFF55F01),
+                            ),
+                            onPressed: () {
+                              updateStatus(model.orderId!, 2);
+                            },
+                            child: Text('Accept')),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            );
+          } else {
+            return Container();
+          }
+        },
+      ),
+    );
+  }
+}
